acd42c401e80cb5b9a6dd209fbe7d5b06b3e6c44
riparuk <faruqirifa@gmail.com>
2023-12-28 03:23:44 +0700

    Practice01 Done

--- Practice01/README.md
+++ Practice01/README.md
@@ -1,3 +1,30 @@
+## Practice01
+Add window, font, shapes(Rectangle and Circle) by read the configuration file, and add bounce if it touches the edge of the window.
+
+![Alt Text](solution.gif)
+
+### Running
+
+1. Make sure you have [g++](https://gcc.gnu.org/) (C++ compiler) and [SFML](https://www.sfml-dev.org/) installed on your system. If not, install them first.
+
+2. Open a terminal or command prompt and navigate to the directory where the Makefile and `main.cpp` file are located using `cd`.
+
+3. Run the following command in the terminal to compile:
+
+    ```bash
+    make compile
+    ```
+
+5. After the command is executed, the result will be stored in a file named `sfmlgame` (as specified by the `-o` option). You can run the compiled program by typing:
+
+    ```bash
+    ./sfmlgame
+    ```
+
+    Make sure to be in the same directory as the `sfmlgame` file or provide the full path if you are in a different directory.
+
+
+### Configuration
 Configuration files are stored in a file called `config.txt`
 
 Each line of the configration file specifies one of the config settings, with the first string in each line being the type of setting that rest of the line specified. Line in the config file can be one of the following types, and `lines can appear in any order in the file`.
@@ -10,12 +37,12 @@ Each line of the configration file specifies one of the config settings, with th
 - This line defines the font which is to be used to draw text to program. `F` is the font file location(it will have no space), `S` is the font size must be int, `R G B` are the color for the font in RGB format and must be in int. 
 - Example : `Font fonts/tech.ttf 18 255 255 255`
 
-`Rectangle N X Y SX SY R G B W M`
+`Rectangle N X Y SX SY R G B W H`
 - Defines a Rectangle Shape where `N` is name of rectangle, `X Y` is initial position of rectangle and must be in float, `SX SY` is initial speed of x and y line and it is must be in float too. `R G B` are the color for the font in RGB format and must be in int. `W` and `H` must be int and are numbers for Width and Height. 
 - Example : `Rectangle RRed 200 200 0.1 0.15 255 0 0 50 25`
 
 `Circle N X Y SX SY R G B r`
-- Defines a Circle Shape where `N` is name of circle, `X Y` is initial position of circle and must be in float, `SX SY` is initial speed of x and y line and it is must be in float too. `R G B` are the color for the font in RGB format and must be in int. `r` is Radius of the circle. 
+- Defines a Circle Shape where `N` is name of circle, `X Y` is initial position of circle and must be in float, `SX SY` is initial speed of x and y line and it is must be in float too. `R G B` are the color for the font in RGB format and must be in int. `r` is Radius of the circle must be int. 
 - Example : `Circle CBlue 200 200 0.02 0.04 0 0 255 100`
 
 
--- Practice01/main.cpp
+++ Practice01/main.cpp
@@ -1,43 +1,149 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Shape.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Keyboard.hpp>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <stdio.h>
+// #include <vector>
+
+class NewShape
+{
+	private:
+		sf::Vector2f speed;
+		
+	public:
+		sf::Text label;
+
+		void setSpeed(float X, float Y)
+		{
+			speed.x = X;
+			speed.y = Y;
+		}
+
+		const sf::Vector2f & getSpeed()
+		{
+			return speed;
+		}	
+};
+
+class NewCircle: public NewShape, public sf::CircleShape
+{};
+
+class NewRectangle: public NewShape, public sf::RectangleShape
+{};
+
+
 
 int main()
 {
-		int wLength = 640;
-		int wHeight = 480;
-    sf::RenderWindow window(sf::VideoMode(wLength, wHeight), "SFML works!");
-		window.setFramerateLimit(60);
-    sf::CircleShape shape(50);
-
-		int r = 0;
-		int g = 255;
-		int b = 255;
-
-    shape.setFillColor(sf::Color(r,g,b));
-		shape.setPosition(300.0f, 300.0f);
-		float moveSpeed = 1.0f;
-
+		int W, H;	//Width and Height of Window
+		sf::RenderWindow window;
 		sf::Font font;
-		if (!font.loadFromFile("/home/ripa/.local/share/fonts/OpenSans-SemiBold.ttf"))
-		{
-				// error...
-			std::cerr << "Could not load font!\n";
-			exit(-1);
-		}
 		sf::Text text;
+		std::vector<NewCircle> circles;
+		std::vector<NewRectangle> rectangles;
 
-		// select the font
-		text.setFont(font); // font is a sf::Font
+		std::string textLine, firstWord;
+		std::ifstream conf("config.txt");
 
-		// set the string to display
-		text.setString("Hello world");
+		// Use a while loop together with the getline() function to read the file line by line
+		while (getline (conf, textLine)) {
+		  // Output the text from the file
+			std::istringstream iss(textLine);
 
-		// set the character size
-		text.setCharacterSize(24); // in pixels, not points!
+			// if line null just skip
+			if (iss.rdbuf()->in_avail() == 0) continue;
+			
+			iss >> firstWord;
 
-		text.setPosition(0, wHeight - (float)text.getCharacterSize()); 
+			if (firstWord == "Window")
+			{
+				iss >> W >> H;
+				window.create(sf::VideoMode(W, H), "Practice 01");
+				window.setFramerateLimit(60);
+				std::cout << "Window created successfully\n";
+			}
+			else if (firstWord == "Font")
+			{
+				std::string F;
+				int S, R, G, B;
+				iss >> F >> S >> R >> G >> B;
+				if (!font.loadFromFile(F))
+				{
+					// error...
+					std::cerr << "Could not load font: " << F << "\n";
+					exit(-1);
+				}
+				// select the font
+				text.setFont(font); // font is a sf::Font
+				
+				// set the character size
+				text.setCharacterSize(S); // in pixels, not points!
+				text.setFillColor(sf::Color(R, G, B));
 
+				std::cout << "Font loaded successfully\n";
+			}
+			else if (firstWord == "Circle")
+			{
+				std::string N;
+				float X, Y, SX, SY;
+				int R, G, B, r;		// r is radius
+
+				iss >> N >> X >> Y >> SX >> SY >> R >> G >> B >> r;
+				NewCircle circle;
+				circle.label = text;
+				circle.label.setString(N);
+				circle.setPosition(X, Y);
+				circle.setSpeed(SX, SY);
+				circle.setFillColor(sf::Color(R, G, B));	
+				circle.setRadius(r);
+
+				//push to vector list
+				circles.push_back(circle);
+				std::cout << "Circle loaded successfully\n";
+			}
+			else if (firstWord == "Rectangle")
+			{
+				std::string N;
+				float X, Y, SX, SY;
+				int R, G, B, width, height;		// r is radius
+
+				iss >> N >> X >> Y >> SX >> SY >> R >> G >> B >> width >> height;
+				NewRectangle rectangle;
+				rectangle.label = text;
+				rectangle.label.setString(N);
+				rectangle.setPosition(X, Y);
+				rectangle.setSpeed(SX, SY);
+				rectangle.setFillColor(sf::Color(R, G, B));	
+				rectangle.setSize(sf::Vector2f(width, height));
+
+				//push to vector list
+				rectangles.push_back(rectangle);
+				std::cout << "Rectangle loaded successfully\n";
+			} 
+			else
+			{
+				std::cerr << "Wrong config format, check again!\n";
+				exit(-1);
+			}
+
+
+		}	
+
+		for (auto &i : rectangles)
+		{
+			std::string name = i.label.getString();
+			std::cout << name << "\n";
+		}
+
+		float direction = 1.0f;
     while (window.isOpen())
     {
         sf::Event event;
@@ -50,19 +156,77 @@ int main()
 						{
 							std::cout << "Key pressed with code : " << event.key.code << "\n";
 
-							//example if X is pressed
+							//if x is pressed the direction will change and speed up 2x 
+							//if z is pressed the direction will change and slow down by 0.5x
 							if (event.key.code == sf::Keyboard::X)
 							{
-								moveSpeed *= -1.0f;
+								direction *= -2.0f;
+							}
+							if (event.key.code == sf::Keyboard::Z)
+							{
+								direction *= -0.5f;
 							}
 						}
         }
-				shape.setPosition(shape.getPosition().x + moveSpeed, shape.getPosition().y + moveSpeed);
-				std::cout << "moveSpeed : " << moveSpeed << "\n";
 
         window.clear();
-        window.draw(shape);
-				window.draw(text);
+
+				for (auto & rectangle : rectangles)
+				{
+				 	rectangle.setPosition(rectangle.getPosition().x + rectangle.getSpeed().x * direction, rectangle.getPosition().y + rectangle.getSpeed().y * direction);
+
+					//if touch x edge of the window, rectangle will bounce
+					if (rectangle.getPosition().x <= 0 || rectangle.getPosition().x + rectangle.getSize().x > window.getSize().x)
+					{
+						rectangle.setSpeed(rectangle.getSpeed().x * -1.0f, rectangle.getSpeed().y);
+						std::string name = rectangle.label.getString();
+						std::cout << name <<  " Collisioned x \n";
+					} 
+					// if touch y edge of the window, rectangle will bounce
+					if (rectangle.getPosition().y <= 0 || rectangle.getPosition().y + rectangle.getSize().y > window.getSize().y)
+					{
+						rectangle.setSpeed(rectangle.getSpeed().x, rectangle.getSpeed().y * -1.0f);
+						std::string name = rectangle.label.getString();
+						std::cout << name <<  " Collisioned y \n";
+					}
+
+					//center text to rect
+					auto text = rectangle.label;
+					text.setOrigin(text.getGlobalBounds().getSize() / 2.f + text.getLocalBounds().getPosition());
+					text.setPosition(rectangle.getPosition() + (rectangle.getSize() / 2.f));
+
+					window.draw(rectangle);
+					window.draw(text);
+				}
+			
+				for (auto & circle : circles)
+				{
+				 	circle.setPosition(circle.getPosition().x + circle.getSpeed().x * direction, circle.getPosition().y + circle.getSpeed().y * direction);
+
+					//if touch x edge of the window, rectangle will bounce
+					if (circle.getPosition().x <= 0 || circle.getPosition().x + circle.getRadius()*2 > window.getSize().x)
+					{
+						circle.setSpeed(circle.getSpeed().x * -1.0f, circle.getSpeed().y);
+						std::string name = circle.label.getString();
+						std::cout << name <<  " Collisioned x \n";
+					} 
+					//if touch y edge of the window, rectangle will bounce
+					if (circle.getPosition().y <= 0 || circle.getPosition().y + circle.getRadius()*2 > window.getSize().y)
+					{
+						circle.setSpeed(circle.getSpeed().x, circle.getSpeed().y * -1.0f);
+						std::string name = circle.label.getString();
+						std::cout << name <<  " Collisioned x \n";
+					}
+
+					//center text to rect
+					auto text = circle.label;
+					text.setOrigin(text.getGlobalBounds().getSize() / 2.f + text.getLocalBounds().getPosition());
+					text.setPosition(circle.getPosition() + sf::Vector2f(circle.getRadius(), circle.getRadius()));
+
+				 	window.draw(circle);
+					window.draw(text);
+				}
+
         window.display();
     }
 
